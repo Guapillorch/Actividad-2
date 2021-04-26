4e13ec299dc5c0d8c862937b0415ae89d6381200
Ramirez Chico Harold <82553406+Guapillorch@users.noreply.github.com>
2021-04-26 18:34:01 -0500

    Add files via upload

---
+++ Programa5.c
@@ -0,0 +1,23 @@
+# incluye   stdio.h
+
+int  main ()
+[
+    / * Ramirez Chico Harold
+    Equipo 1
+    21 abr 21
+    Programa 5. Pide el Area de un tringulo
+    * /
+   
+   flotador base, altura, imprimación, segundo;
+   printf ( " \ n \ n Programa 5 " );
+   printf ( " \ n \ n Saca el promedio de valores reales " );
+   printf ( " \ n \ n Dame el primer valor " );
+   scanf ( " % f " , & cebador); // Permite capturar un valor real y lo guarda en variable //
+   printf ( " \ n \ n Dame el segundo valor " );
+   scanf ( " % f " , & segundo); // Permite capturar un valor real y lo guarda en variable //
+   Área Base * altura / dos;
+   printf   ( " \ n El producto de la base que multiplica por la altura entre 2 es & f " );
+   printf   ( " \ n el priducto que dicide es % f   );
+    
+    return 0;
+]
---
+++ Programa6.c
@@ -0,0 +1,26 @@
+# incluye   stdio.h
+
+int  main ()
+{ 
+    / * Ramirez Chico Harold
+    Equipo 1
+    21 abr 21
+    Programa 6. Pide el volumen del trapecio
+    * /
+   
+   float basemayor, basemenor, altura, primero, segundo, tercero;
+   printf ( " \ n \ n Programa 5 " );
+   printf ( " \ n \ n Saca el promedio de valores reales " );
+   printf ( " \ n \ n Dame el primer valor " );
+   scanf ( " % f " , & cebador); // Permite capturar un valor real y lo guarda en variable //
+   printf ( " \ n \ n Dame el segundo valor " );
+   scanf ( " % f " , & segundo); // Permite capturar un valor real y lo guarda en variable //
+   printf ( " \ n \ n Dame el tercero valor " );
+   scanf ( " % f " , & tercero); // Permite capturar un valor real y lo guarda en variable //
+
+   Volumen Basemayor + basemenor * altura / dos;
+   printf   ( " \ n El producto de base mayor mas base menor multiplicando la altura entre dos es & f " );
+   printf   ( " \ n el priducto que dicide es % f   );
+    
+    return 0;
+}
